8772d4e3a659910a839c87c9490d2a1e0cf77a3d
“Prerna <pkakkar95@gmail.com>
2016-08-26 15:10:21 +0530

    removed switcher

--- index.php
+++ index.php
@@ -25,24 +25,7 @@
 
 <div class="layout-theme animated-css"  data-header="sticky" data-header-top="200">
 
-  <!-- Start Switcher -->
-  <div class="switcher-wrapper">
-    <div class="demo_changer">
-      <div class="demo-icon customBgColor"><i class="fa fa-cog fa-spin fa-2x"></i></div>
-      <div class="form_holder">
-        <div class="row">
-          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <div class="predefined_styles">
-              <div class="skin-theme-switcher">
-                <h4>Color</h4>
-                <a href="javascript:void(0);" data-switchcolor="color1" class="styleswitch" style="background-color:#fe5656;"> </a> <a href="javascript:void(0);" data-switchcolor="color2" class="styleswitch" style="background-color:#4fb0fd;"> </a> <a href="javascript:void(0);" data-switchcolor="color3" class="styleswitch" style="background-color:#ffc73c;"> </a> <a href="javascript:void(0);" data-switchcolor="color4" class="styleswitch" style="background-color:#ff8300;"> </a> <a href="javascript:void(0);" data-switchcolor="color5" class="styleswitch" style="background-color:#02cc8b;"> </a> </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- End Switcher -->
+ 
 
   <div id="wrapper">
 
