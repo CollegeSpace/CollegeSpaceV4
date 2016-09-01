1013f030821dcc3606c0893077af412c741d87ff
JeeveshN <narang.jeevesh@gmail.com>
2016-09-02 01:11:04 +0530

    Tweet widget minor fixes

--- assets/footer.php
+++ assets/footer.php
@@ -29,7 +29,7 @@
                     </ul>
                   </section><!-- end footer-section -->
                 </div>
-                <div class="col-lg-4 col-sm-4" id="tweet">
+                <div class="col-lg-4 col-sm-4">
                     <section class="footer-section">
                         <a class="twitter-timeline" data-tweet-limit="3" data-theme="dark" data-chrome="transparent"   href="https://twitter.com/college_space">Tweets by college_space</a>
                     </section>
