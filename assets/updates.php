f019b8f12b4c7b9f753dcfcc923f7095ac53f8a0
ayushgarg1804 <ayushgarg1804@gmail.com>
2016-09-04 19:14:29 +0530

    Removed images and made grid 3x2 from updates section (#41)
    Should fix #40

--- assets/updates.php
+++ assets/updates.php
@@ -1,7 +1,7 @@
 <?php
 
   require_once("relative_time.php");
-  $url="http://updates.collegespace.in/wp-json/posts?filter[posts_per_page]=4";
+  $url="http://updates.collegespace.in/wp-json/posts?filter[posts_per_page]=6";
 
   $context = stream_context_create([
         "http" => [
@@ -16,20 +16,12 @@
     $result=json_decode($json);
     //var_dump($result);
 
-    for($x=0;$x<4;$x++)
+    for($x=0;$x<6;$x++)
     {
       $data=$result[$x];
       //var_dump($data);
       // $doc = new DOMDocument();
       //$doc->loadHTML($data->content);
-      $img_src="assets/img/logo_256x256.png";
-      $img_tag;$img;
-      if(preg_match("/<img[^>]+\>/i",$data->content,$img_tag))
-      {
-        preg_match('/\<img.+src\=(?:\"|\')(.+?)(?:\"|\')(?:.+?)\>/',$img_tag[0],$img);
-        $img_src=$img[1];
-      }
-       // var_dump($img_src);
       $data->content = preg_replace("/<img[^>]+\>/i", "", $data->content);
       $data->content = preg_replace("/<table[^>]*>.*?<\/table>/s", "", $data->content);
       $data->content=strip_tags($data->content);
@@ -39,9 +31,6 @@
         $data->content="No description available.";
       $rel_time=time2str($data->date);
       echo"<article class=\"post post_mod-a clearfix wow \" data-wow-duration=\"1s\">
-                    <div class=\"entry-media\">
-                       <div class=\"entry-thumbnail\"><img class=\"img-responsive\" src={$img_src} alt=\"CollegeSpace\"/></div>
-                    </div>
                     <div class=\"entry-main\">
                       <div class=\"entry-meta\"> <span class=\"entry-autor\"> <span>By </span> <a class=\"post-link\" href=\"javascript:void(0);\">CollegeSpace,NSIT</a> </span>
                       <br><span class=\"entry-date\"><a href=\"javascript:void(0);\">{$rel_time}</a></span> </div>
