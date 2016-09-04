229d677830a930ee04a6f712a2df969347852978
Divyanshu Kalra <kalradivyanshu@gmail.com>
2016-09-04 20:38:42 +0530

    Made UI changes, common navbar, solved stupid typo

--- search-page.php
+++ search-page.php
@@ -85,7 +85,7 @@ else
 <!DOCTYPE html>
 <html lang="en">
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-<head><?php PrintHeadMetadata("CollegeSpace | Search Page"); ?></head>s
+<head><?php PrintHeadMetadata("CollegeSpace | Search Page"); ?></head>
 <body>
 <!-- Loader -->
 <!--<div id="page-preloader"><span class="spinner"></span></div>-->
