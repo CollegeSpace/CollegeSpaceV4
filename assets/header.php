a4a1fb3b7d766e693a588ffebf0a3a0208289171
mansimarkaur <mansimarkaur.mks@gmail.com>
2016-12-04 02:24:30 +0530

    navbar, searchbar and logo placement in mobile view

--- assets/header.php
+++ assets/header.php
@@ -1,7 +1,8 @@
 <header class="header sticky">
     <div class="container">
         <div class="row">
-            <div class="col-xs-12"> <a class="header-logo" href="javascript:void(0);"><img class="header-logo__img" src="assets/img/logo.png" alt="Logo"></a>
+            <div class="col-xs-12">
+                <a class="header-logo" href="javascript:void(0);"><img class="header-logo__img" src="assets/img/logo.png" alt="Logo"></a>
                 <div class="header-inner">
                     <div class="header-search">
                         <div class="navbar-search">
