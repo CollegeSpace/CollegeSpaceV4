b7c3a1ef04b55ee11be8c3ad49c2fee7cb2d44df
Rishab Gupta <rishab0611@gmail.com>
2017-01-26 21:04:51 +0530

    index page issues on chrome

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <body>
 
   <div id="page-preloader">
-  	  <div id="loading-center">
+  	  <div>
   			  <h1 id="loading-center" class="preloader_heading animated bounceInDown">CollegeSpace</h1>
   				<div id="loading-center-absolute">
   					  <div class="object" id="object_one"></div>
@@ -95,6 +95,7 @@
             <h3 class="ui-title-inner decor decor_mod-a">NSITPEDIA</h3>
             <div class="advantages__info">
               <p>The ultimate destination to explore NSIT! A single platform that presents a humongous pack of movie and food reviews, latest technological releases, thought provoking poetries and the latest trends in the campus.</p>
+            </div>
             </div>
           </li>
           <li class="advantages__item"> <span class="advantages__icon"><i class="icon stroke icon-Users"></i></span>
@@ -183,25 +184,6 @@
     </section>
     <!-- end section-default -->
 
-    <!--div class="section-progress wow  section-parallax"  data-speed="25"  data-wow-duration="2s">
-    <div class="container">
-      <div class="row">
-      <div class="col-xs-12">
-        <ul class="list-progress list-unstyled">
-        <li class="list-progress__item"> <span class="chart label-chart decor decor_mod-c" data-percent="190"><i class="icon stroke icon-WorldWide"></i><span class="percent"></span> </span> <span class="list-progress__name">COUNTRIES REACHED</span> </li>
-        <li class="list-progress__item"> <span class="chart label-chart decor decor_mod-c" data-percent="250000"><i class="icon stroke icon-Book"></i><span class="percent"></span> </span> <span class="list-progress__name">PASSED GRADUATES</span> </li>
-        <li class="list-progress__item"> <span class="chart label-chart decor decor_mod-c" data-percent="824"><i class="icon stroke icon-User"></i><span class="percent"></span> </span> <span class="list-progress__name">QUALIFIED STAFF</span> </li>
-        <li class="list-progress__item"> <span class="chart label-chart decor decor_mod-c" data-percent="1500"><i class="icon stroke icon-Edit"></i><span class="percent"></span> </span> <span class="list-progress__name">COURSES PUBLISHED</span> </li>
-        </ul>
-        <!-- end list-progress >
-      </div>
-      <!-- end col  >
-      </div>
-      <!-- end row  >
-    </div>
-    <!-- end container>
-    </div>
-    <!-- end section-progress-->
 
     <section class="section-default">
     <div class="container">
@@ -234,18 +216,18 @@
           <div class="subscribe__description">Nulla feugiat nibh placerat fermentum rutrum ante risus euismod</div>
         </div>
         </div>
-        <!-- end col --
+        
         <div class="col-sm-6">
         <form class="subscribe__form" action="http://html.templines.com/academica/get">
           <input class="subscribe__input form-control" type="text" placeholder="Your Email address ...">
           <button class="subscribe__btn btn btn-success btn-effect">SUBSCRIBE</button>
         </form>
         </div>
-        <!-- end col --
+        
       </div>
-      <!-- end row --
+      
       </div>
-      <!-- end container --
+      
     </div>
     </section>-->
     <!-- end section-subscribe -->
